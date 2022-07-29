136789c3beca682c7a9a0bb0ad7edbad1eb5fda0
Olav Vahtras <vahtras@kth.se>
2022-07-30 01:07:22 +0200

    initial digit properties

--- digits.py
+++ digits.py
@@ -1,3 +1,15 @@
 class Digit:
+    maps = {
+        8: (0, 1, 2, 3, 4, 5, 6)
+    }
+    removals = {
+        8: {0, 6, 9}
+    }
     def __init__(self, value):
         self.value = value
+
+    def __len__(self):
+        return len(self.maps[self.value])
+
+    def removal_values(self):
+        return self.removals[self.value]
--- test_digit.py
+++ test_digit.py
@@ -1,5 +1,22 @@
+import pytest
+
 from digits import Digit
 
-def test_eight():
+@pytest.mark.parametrize(
+    'value',
+    [
+        (0,),
+        (8,),
+    ]
+) 
+def test_eight_value(value):
+    eight = Digit(value)
+    assert eight.value == value
+
+def test_eight_len():
     eight = Digit(8)
-    assert eight.value == 8
+    assert len(eight) == 7
+
+def test_eight_removals():
+    eight = Digit(8)
+    assert eight.removal_values() == {0, 6, 9}
